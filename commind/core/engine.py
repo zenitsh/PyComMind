b69241937f1aca7cdc0b6fe1701e967fb39331f4
zwhsh <zhang_wh2020@163.com>
2021-09-09 16:41:51 +0800

    basic function enabled

---
+++ commind/core/engine.py
@@ -0,0 +1,9 @@
+from commind.core.node import Node
+from commind.v.arrange import *
+
+class Engine:
+    def __init__(self):
+        self.root = Node()
+
+    def refresh():
+        arrangeNodeByRectangle()
